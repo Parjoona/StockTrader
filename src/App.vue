85a5079ad05229955d0e1458a252ca7713564285
Unknown <parjoona@hotmail.com>
2018-07-06 13:48:06 +0200

    Added slide animation between routes

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
         <app-header></app-header>
         <div class="row">
             <div class="col-12">
-                <router-view></router-view>
+                <transition name="slide" mode="out-in">
+                    <router-view></router-view>
+                </transition>
             </div>
         </div>
     </div>
@@ -23,5 +25,31 @@
 </script>
 
 <style>
+    .slide-enter-active {
+        animation: slide-in 200ms ease-out forwards;
+    }
 
+    .slide-enter-leave {
+        animation: slide-out 200ms ease-out forwards;
+    }
+
+    @keyframes slide-in {
+        from {
+            transform: translateX(-30px);
+            opacity: 0;
+        } to {
+            transform: translateX(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes slide-out {
+        from {
+            transform: translateX(0);
+            opacity: 1;
+        } to {
+            transform: translateX(30px);
+            opacity: 0;
+        }
+    }
 </style>
